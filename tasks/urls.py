d930f4f4fd2fc16ae0b22f4c667b134b557deeb3
Pellizzon <matheuspellizzon@hotmail.com>
2020-11-10 19:07:23 -0300

    http methods

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('getTasks', views.getTasks, name='AllTasks')
+    path("", views.index, name="index"),
+    path("list", views.getTasks, name="AllTasks"),
+    path("<int:task_id>", views.getTask, name="SingleTask"),
+    path("create", views.createTask, name="CreateTask"),
+    path("delete/<int:task_id>", views.deleteTask, name="DeleteTask"),
+    path("deleteAll", views.deleteTasks, name="DeleteAllTask"),
+    path("update/<int:task_id>", views.updateTask, name="UpdateTask"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,80 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from tasks.models import Task
 from django.core import serializers
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from tasks.serializer import TaskSerializer
 
 
 # Create your views here.
 
 
+@api_view(["GET"])
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 
 
+@api_view(["GET"])
 def getTasks(request):
     allTasks = Task.objects.all()
-    _json = serializers.serialize("json", allTasks)
 
+    _json = serializers.serialize("json", allTasks)
+    # return
     return HttpResponse(_json, content_type="application/json")
+
+
+@api_view(["GET"])
+def getTask(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+        return JsonResponse(model_to_dict(task), safe=False)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+
+
+@api_view(["POST"])
+def createTask(request):
+    data = JSONParser().parse(request)
+    taskSerializer = TaskSerializer(data=data)
+    if taskSerializer.is_valid():
+        taskSerializer.save()
+        return JsonResponse(taskSerializer.data, status=201)
+    return JsonResponse(taskSerializer.errors, status=400)
+
+
+@api_view(["PUT"])
+def updateTask(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except:
+        raise Http404("Task does not exist")
+
+    current_task = model_to_dict(task)
+    data = JSONParser().parse(request)
+
+    for i in data:
+        current_task[i] = data[i]
+
+    taskSerializer = TaskSerializer(data=current_task)
+    if taskSerializer.is_valid():
+        taskSerializer.save()
+        return JsonResponse(taskSerializer.data, status=201)
+    return JsonResponse(taskSerializer.errors, status=400)
+
+
+@api_view(["DELETE"])
+def deleteTask(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+        task.delete()
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    return HttpResponse("Task deleted succesfully")
+
+
+@api_view(["DELETE"])
+def deleteTasks(request):
+    task = Task.objects.all().delete()
+    return HttpResponse("All tasks deleted succesfully")
