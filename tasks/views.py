283ee738e62af25add1c511d5f85202be35edc12
Pellizzon <38332600+Pellizzon@users.noreply.github.com>
2020-11-10 16:55:45 -0300

    test getTasks view

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.models import Task
+from django.core import serializers
 
 
 # Create your views here.
@@ -7,3 +9,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+def getTasks(request):
+    allTasks = Task.objects.all()
+    _json = serializers.serialize("json", allTasks)
+
+    return HttpResponse(_json, content_type="application/json")
